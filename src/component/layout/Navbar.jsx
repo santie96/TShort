16a2c06fc73a82d719fa5549b89df1a51787f128
santie96 <santi96emma@gmail.com>
2026-07-02 18:24:09 +0200

    fix navbar

--- src/component/layout/Navbar.jsx
+++ src/component/layout/Navbar.jsx
@@ -1,7 +1,7 @@
 import { PiTShirtFill } from "react-icons/pi";
 import { NavLink } from "react-router-dom";
 import { RxHamburgerMenu } from "react-icons/rx";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { IoSearch } from "react-icons/io5";
 import { PiMoonThin } from "react-icons/pi";
 import { FaRegHeart } from "react-icons/fa";
@@ -13,6 +13,13 @@ function Navbar() {
 
     const [isMenuOpen, setIsMenuOpen] = useState(false);
 
+    useEffect(() => {
+        document.body.style.overflow = isMenuOpen ? "hidden" : "";
+        return () => {
+            document.body.style.overflow = "";
+        };
+    }, [isMenuOpen]);
+
     return (
         <>
             <header className="relative w-full bg-[#F6F4F0] flex items-center">
@@ -20,101 +27,102 @@ function Navbar() {
                     <div className="bg-[#211D1A] py-2">
                         <p className="text-[#F6F4F0] font-text text-[0.75rem] w-full h-full m-0">Spedizione gratuita sopra i 49€ · Reso gratuito entro 30 giorni</p>
                     </div>
-                    <div className=" flex items-center justify-between px-5 py-4 w-full">
+                    <div>
+                        <div className="flex items-center justify-between px-5 py-4 w-full">
 
-                        <div className="group">
-                            <RxHamburgerMenu
-                                onClick={() => setIsMenuOpen(prev => !prev)}
-                                className="z-50 text-[#211D1A] text-xl cursor-pointer transition-transform duration-300 group-hover:scale-110"
-                            />
+                            <div className="group">
+                                <RxHamburgerMenu
+                                    onClick={() => setIsMenuOpen(prev => !prev)}
+                                    className="z-50 text-[#211D1A] text-xl cursor-pointer transition-transform duration-300 group-hover:scale-110"
+                                />
 
-                            <div
-                                className={`absolute top-0 left-0 z-50 w-75 h-dvh bg-[#F6F4F0] rounded-b-2xl flex flex-col transition-all duration-300 ease-in-out ${isMenuOpen
-                                    ? "translate-x-0 opacity-100"
-                                    : "-translate-x-full opacity-0 pointer-events-none"
-                                    }`}
-                            >
-                                <div className="flex items-center justify-between p-5">
-                                    <div>
-                                        <h2 className="font-semibold text-[#211D1A] font-title text-2xl">TSHORT</h2>
+                                <div
+                                    className={`absolute top-0 left-0 z-50 w-75 h-dvh bg-[#F6F4F0] rounded-b-2xl flex flex-col transition-all duration-300 ease-in-out ${isMenuOpen
+                                        ? "translate-x-0 opacity-100"
+                                        : "-translate-x-full opacity-0 pointer-events-none"
+                                        }`}
+                                >
+                                    <div className="flex items-center justify-between p-5">
+                                        <div>
+                                            <h2 className="font-semibold text-[#211D1A] font-title text-2xl">TSHORT</h2>
+                                        </div>
+                                        <div className="flex gap-3 text-xl">
+                                            <PiMoonThin className="cursor-pointer" />
+                                            <IoClose className="cursor-pointer" onClick={() => setIsMenuOpen(false)} />
+                                        </div>
                                     </div>
-                                    <div className="flex gap-3 text-xl">
-                                        <PiMoonThin className="cursor-pointer" />
-                                        <IoClose className="cursor-pointer" onClick={() => setIsMenuOpen(false)} />
-                                    </div>
+
+                                    <hr className="text-gray-400" />
+
+                                    <ul className="flex flex-col items-start w-full px-4 pt-8 gap-9 text-slate-200 font-medium">
+                                        <li>
+                                            <NavLink
+                                                className="text-[#211D1A] hover:bg-slate-600 flex items-center justify-center"
+                                                to="/catalogo"
+                                                onClick={() => setIsMenuOpen(false)}
+                                            >
+                                                Catalogo
+                                            </NavLink>
+                                        </li>
+
+                                        <li>
+                                            <NavLink
+                                                className="text-[#211D1A] hover:bg-slate-600 flex items-center justify-center"
+                                                to="/abbigliamento-uomo"
+                                                onClick={() => setIsMenuOpen(false)}
+                                            >
+                                                Uomo
+                                            </NavLink>
+                                        </li>
+
+                                        <li>
+                                            <NavLink
+                                                className="text-[#211D1A] hover:bg-slate-600 flex items-center justify-center"
+                                                to="/abbigliamento-donna"
+                                                onClick={() => setIsMenuOpen(false)}
+                                            >
+                                                Donna
+                                            </NavLink>
+                                        </li>
+
+                                        <li>
+                                            <NavLink
+                                                className="text-[#211D1A] hover:bg-slate-600 flex items-center justify-center"
+                                                to="/abbigliamento-bambini"
+                                                onClick={() => setIsMenuOpen(false)}
+                                            >
+                                                Bambini
+                                            </NavLink>
+                                        </li>
+                                        <li>
+                                            <NavLink
+                                                className="hover:bg-slate-600 text-red-600 flex items-center justify-center"
+                                                to="/saldi"
+                                                onClick={() => setIsMenuOpen(false)}
+                                            >
+                                                Saldi
+                                            </NavLink>
+                                        </li>
+                                    </ul>
                                 </div>
-
-                                <hr className="text-gray-400" />
-
-                                <ul className="flex flex-col items-start w-full px-4 pt-8 gap-9 text-slate-200 font-medium">
-                                    <li>
-                                        <NavLink
-                                            className="text-[#211D1A] hover:bg-slate-600 flex items-center justify-center"
-                                            to="/catalogo"
-                                            onClick={() => setIsMenuOpen(false)}
-                                        >
-                                            Catalogo
-                                        </NavLink>
-                                    </li>
-
-                                    <li>
-                                        <NavLink
-                                            className="text-[#211D1A] hover:bg-slate-600 flex items-center justify-center"
-                                            to="/abbigliamento-uomo"
-                                            onClick={() => setIsMenuOpen(false)}
-                                        >
-                                            Uomo
-                                        </NavLink>
-                                    </li>
-
-                                    <li>
-                                        <NavLink
-                                            className="text-[#211D1A] hover:bg-slate-600 flex items-center justify-center"
-                                            to="/abbigliamento-donna"
-                                            onClick={() => setIsMenuOpen(false)}
-                                        >
-                                            Donna
-                                        </NavLink>
-                                    </li>
-
-                                    <li>
-                                        <NavLink
-                                            className="text-[#211D1A] hover:bg-slate-600 flex items-center justify-center"
-                                            to="/abbigliamento-bambini"
-                                            onClick={() => setIsMenuOpen(false)}
-                                        >
-                                            Bambini
-                                        </NavLink>
-                                    </li>
-                                    <li>
-                                        <NavLink
-                                            className="hover:bg-slate-600 text-red-600 flex items-center justify-center"
-                                            to="/saldi"
-                                            onClick={() => setIsMenuOpen(false)}
-                                        >
-                                            Saldi
-                                        </NavLink>
-                                    </li>
-                                </ul>
                             </div>
-                        </div>
 
-                        <div>
-                            <NavLink className="flex items-center gap-3 text-slate-100">
-                                <h2 className="text-[1.5rem] font-semibold text-[#211D1A] font-title">TSHORT</h2>
-                            </NavLink>
-                        </div>
+                            <div>
+                                <NavLink className="flex items-center gap-3 text-slate-100">
+                                    <h2 className="text-[1.5rem] font-semibold text-[#211D1A] font-title">TSHORT</h2>
+                                </NavLink>
+                            </div>
 
-                        <div className="flex gap-4 text-xl">
-                            <IoSearch className="cursor-pointer" />
-                            <PiMoonThin className="cursor-pointer" />
-                            <FaRegHeart className="cursor-pointer" />
-                            <MdOutlineShoppingCart className="cursor-pointer" />
-                        </div>
+                            <div className="flex gap-4 text-xl">
+                                <IoSearch className="cursor-pointer" />
+                                <PiMoonThin className="cursor-pointer" />
+                                <FaRegHeart className="cursor-pointer" />
+                                <MdOutlineShoppingCart className="cursor-pointer" />
+                            </div>
 
 
-                        {/* Visualizzazione per Mobile */}
-                        {/* <nav>
+                            {/* Visualizzazione per Mobile */}
+                            {/* <nav>
                         <ul className="flex ">
                             <li>
                                 <NavLink to="/catalogo">Catalogo</NavLink>
@@ -130,7 +138,8 @@ function Navbar() {
                             </li>
                         </ul>
                     </nav> */}
-                    </div >
+                        </div >
+                    </div>
                 </div>
             </header >
 
