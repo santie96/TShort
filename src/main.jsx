3c34b44e348f78c1d72dc7d2e9ec4a1c53d51ea3
Mattia Formicola <sparkly0598@gmail.com>
2026-07-06 11:50:00 +0200

    fix generale

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import ScrollToTop from "./component/utilities/ScrollToTop.jsx";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </StrictMode>,
-)
+);
