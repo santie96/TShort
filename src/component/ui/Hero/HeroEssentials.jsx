ec331a69d2e9465f9d5acb379d4e087789477107
santie96 <santi96emma@gmail.com>
2026-07-08 18:53:22 +0200

    Feature Card layout and container

--- src/component/ui/Hero/HeroEssentials.jsx
+++ src/component/ui/Hero/HeroEssentials.jsx
@@ -2,11 +2,11 @@ import { NavLink } from "react-router-dom"
 
 function HeroEssentials() {
     return (
-        <section className="relative bg-[url('/src/img/Summer-Essentials.webp')] max-[769px]:bg-bottom bg-cover bg-no-repeat min-h-[55vh] md:min-h-140">
+        <section className="relative bg-[url('./src/img/hero/Summer-Essentials.webp')] max-[769px]:bg-bottom bg-cover bg-no-repeat min-h-[55vh] md:min-h-140">
             <div className="flex flex-col gap-7 justify-center items-center absolute w-full h-full inset-0 bg-black/60">
             <div className="px-5 flex flex-col gap-4 ">
-                <h2 className="text-white font-title text-4xl text-center">Summer Essentials</h2>
-                <p className="m-auto text-white text-center font-text text-size">Leggeri, freschi, versatili</p>
+                <h2 className="text-[#F6F4F0] font-title text-4xl text-center">Summer Essentials</h2>
+                <p className="m-auto text-[#F6F4F0] text-center font-text text-size">Leggeri, freschi, versatili</p>
             </div>
                 <div className="flex gap-5">
                     <button
--- src/component/ui/Hero/HeroNArrivi.jsx
+++ src/component/ui/Hero/HeroNArrivi.jsx
@@ -2,11 +2,11 @@ import { NavLink } from "react-router-dom"
 
 function HeroNArrivi() {
     return (
-        <section className="relative bg-[url('/src/img/Nuovi-Arrivi.webp')] max-[769px]:bg-bottom bg-cover bg-no-repeat min-h-[55vh] md:min-h-140">
+        <section className="relative bg-[url('./src/img/hero/Nuovi-Arrivi.webp')] max-[769px]:bg-bottom bg-cover bg-no-repeat min-h-[55vh] md:min-h-140">
             <div className="flex flex-col gap-7 justify-center items-center absolute w-full h-full inset-0 bg-black/60">
             <div className="px-5 flex flex-col gap-4 ">
-                <h2 className="text-white font-title text-4xl text-center">Nuova collezione Estate</h2>
-                <p className="m-auto text-white text-center font-text text-size">Il tuo look, nuova energia.</p>
+                <h2 className="text-[#F6F4F0] font-title text-4xl text-center">Nuova collezione Estate</h2>
+                <p className="m-auto text-[#F6F4F0] text-center font-text text-size">Il tuo look, nuova energia.</p>
             </div>
                 <div className="flex gap-5">
                     <button
