1d92651ba3147b3dec1634c2ff9ba8b800d939c2
Mattia Formicola <sparkly0598@gmail.com>
2026-07-09 12:37:55 +0200

    fix/generali

--- src/component/ui/Card/Card.jsx
+++ src/component/ui/Card/Card.jsx
@@ -1,27 +1,29 @@
 import { NavLink } from "react-router-dom";
 import { MdOutlineArrowOutward } from "react-icons/md";
 
-
-function Card() {
-
-    return (
-        <>
-            <NavLink to="/abbigliamento-uomo" className="relative overflow-hidden rounded-xl">
-                <div className="relative group aspect-[3/4] sm:aspect-[4/5]">
-                    <img src="./src/img/categoria/men.webp" alt="immagine" className="w-full h-full object-cover block transition-all group-hover:scale-105 duration-700 ease-in-out" />
-                    <div className="absolute bottom-0 flex items-end lg:p-5 w-full h-full inset-0 bg-[#191101]/10 justify-between">
-                        <div className="text-[#F6F4F0]">
-                            <h2 className="text-stitle-size font-semibold font-title">Pantalone</h2>
-                            <p className="text-sm font-text">Essenziali versatili per ogni giorno</p>
-                        </div>
-                        <div className="p-2.5 bg-[#F6F4F0] group-hover:bg-[#C47048] group-hover:text-[#F6F4F0] duration-300 rounded-full cursor-pointer flex justify-center items-center">
-                            <MdOutlineArrowOutward className="text-icon-size" />
-                        </div>
-                    </div>
-                </div>
-            </NavLink>
-        </>
-    )
+function Card({ title, subtitle, image, to }) {
+  return (
+    <>
+      <NavLink to={to} className="relative overflow-hidden rounded-xl">
+        <div className="relative group aspect-[3/4] sm:aspect-[4/5] ">
+          <img
+            src={image}
+            alt={title}
+            className="w-full h-full object-cover block transition-all group-hover:scale-105 duration-700 ease-in-out"
+          />
+          <div className="absolute bottom-0 flex items-end p-5 md:p-5 lg:p-5 w-full h-full inset-0 bg-[#191101]/10 justify-between">
+            <div className="text-[#F6F4F0]">
+              <h2 className="text-stitle-size font-semibold font-title">{title}</h2>
+              <p className="text-sm font-text">{subtitle}</p>
+            </div>
+            <div className="p-2.5 bg-[#F6F4F0] group-hover:bg-[#C47048] group-hover:text-[#F6F4F0] duration-300 rounded-full cursor-pointer flex justify-center items-center">
+              <MdOutlineArrowOutward className="text-icon-size" />
+            </div>
+          </div>
+        </div>
+      </NavLink>
+    </>
+  );
 }
 
-export default Card
+export default Card;
--- src/component/ui/Card/ContCards.jsx
+++ src/component/ui/Card/ContCards.jsx
@@ -1,24 +1,47 @@
-import Card from "./Card"
-
+import Card from "./Card";
 
 function ContCard() {
-
-    return (
-        <>
-            <div className="bg-[#F6F4F0] w-full">
-                <div className=" flex flex-col justify-center items-center">
-                    <span className="uppercase text-sm font-text text-[#C47048] mt-8">Fai la tua scelta</span>
-                    <h2 className="font-title text-[#211D1A] font-semibold text-[2.25rem]">Uno stile per tutta la famiglia</h2>
-                    <div className="grid grid-cols-1 md:grid-cols-3 md:p-7 lg:px-55  gap-4">
-                        <Card />
-                        <Card />
-                        <Card />
-                    </div>
-                </div>
+  const categorie = [
+    {
+      title: "Donna",
+      subtitle: "Silhouette morbide e tessuti naturali",
+      image: "./src/img/categoria/women.webp",
+      to: "/abbigliamento-donna",
+    },
+    {
+      title: "Uomo",
+      subtitle: "Essenziali e versatili per ogni giorno",
+      image: "./src/img/categoria/men.webp",
+      to: "/abbigliamento-uomo",
+    },
+    {
+      title: "Bambini",
+      subtitle: "Comfort e calore per i più piccoli",
+      image: "./src/img/categoria/kids.webp",
+      to: "/abbigliamento-bambini",
+    },
+  ];
+  return (
+    <>
+      <div>
+        <div className="bg-[#F6F4F0] w-full">
+          <div className="flex flex-col justify-center items-center">
+            <span className="uppercase text-sm font-text text-[#C47048] mt-8">Fai la tua scelta</span>
+            <h2 className="font-title text-[#211D1A] font-semibold text-title-size text-center lg:text-[2.25rem]">
+              Uno stile per tutta la famiglia
+            </h2>
+            <div className=" px-4 sm:px-6 lg:px-75 py-4 md:py-6">
+              <div className="grid grid-cols-1 md:grid-cols-3 gap-4 md:gap-6">
+                {categorie.map((item) => (
+                  <Card key={item.title} {...item} />
+                ))}
+              </div>
             </div>
-
-        </>
-    )
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default ContCard
+export default ContCard;
