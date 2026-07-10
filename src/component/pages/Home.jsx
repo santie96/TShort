c1b5f5d135c83a03b6075b4ac23b4abf9005dffc
santie96 <santi96emma@gmail.com>
2026-07-10 10:39:20 +0200

    feature/card-prodotti

--- src/component/pages/Home.jsx
+++ src/component/pages/Home.jsx
@@ -1,11 +1,13 @@
-import ContCategorie from "../ui/Card-Categorie/ContCategorie";
 import CarouselHero from "../ui/Hero/CarouselHero";
+import ContCategorie from "../ui/Card-Categorie/ContCategorie";
+import ContProdotti from "../ui/Card-Prodotti/ContProdotti";
 
 function Home() {
   return (
     <>
       <CarouselHero />
       <ContCategorie />
+      <ContProdotti />
     </>
   );
 }
---
+++ src/component/ui/Card-Prodotti/CardProdotti.jsx
@@ -0,0 +1,26 @@
+function CardProdotti() {
+    return (
+        <>
+        <NavLink to={to} className="relative overflow-hidden rounded-xl">
+        <div className="relative group aspect-[3/4] sm:aspect-[4/5] ">
+          <img
+            src={image}
+            alt={title}
+            className="w-full h-full object-cover block transition-all group-hover:scale-105 duration-700 ease-in-out"
+          />
+          <div className="absolute bottom-0 flex items-end p-5 md:p-5 lg:p-5 w-full h-full inset-0 bg-[#191101]/10 justify-between">
+            <div className="text-[#F6F4F0]">
+              <h2 className="text-stitle-size font-semibold font-title">{title}</h2>
+              <p className="text-sm font-text">{subtitle}</p>
+            </div>
+            <div className="p-2.5 bg-[#F6F4F0] group-hover:bg-[#C47048] group-hover:text-[#F6F4F0] duration-300 rounded-full cursor-pointer flex justify-center items-center">
+              <MdOutlineArrowOutward className="text-icon-size" />
+            </div>
+          </div>
+        </div>
+      </NavLink>
+        </>
+    )
+}
+
+export default CardProdotti
