4d0f9042c97c8444c4370af183683c3adca2fd64
santie96 <santi96emma@gmail.com>
2026-07-10 14:57:12 +0200

    fix

--- src/component/ui/Card-Categorie/ContCategorie.jsx
+++ src/component/ui/Card-Categorie/ContCategorie.jsx
@@ -24,14 +24,14 @@ function ContCategorie() {
   return (
     <>
       <div>
-        <div className="bg-[#F6F4F0] w-full px-4 sm:px-6 lg:px-75 py-4 md:py-6">
+        <div className="bg-[#F6F4F0] w-full px-4 sm:px-6 lg:px-50 py-4 md:py-6">
           <div className="flex flex-col justify-center items-center">
             <span className="uppercase text-sm font-text text-[#C47048] mt-8">Fai la tua scelta</span>
             <h2 className="font-title text-[#211D1A] font-semibold text-title-size text-center lg:text-[2.25rem]">
               Uno stile per tutta la famiglia
             </h2>
             <div className="mt-4">
-              <div className="grid grid-cols-1 md:grid-cols-3 gap-4 md:gap-6">
+              <div className="grid grid-cols-1 md:grid-cols-3 gap-4 md:gap-4">
                 {categorie.map((item) => (
                   <CardCategorie key={item.title} {...item} />
                 ))}
--- src/component/ui/Card-Prodotti/ContProdotti.jsx
+++ src/component/ui/Card-Prodotti/ContProdotti.jsx
@@ -5,14 +5,14 @@ function ContProdotti() {
   return (
     <>
       <div>
-        <div className="bg-[#F6F4F0] w-full px-4 sm:px-6 lg:px-75 py-4 md:py-6">
+        <div className="bg-[#F6F4F0] w-full px-4 sm:px-6 lg:px-50 py-4 md:py-6">
           <div className="flex flex-col justify-start items-start">
             <span className="uppercase text-sm font-text text-[#C47048] mt-8">Appena arrivati</span>
             <h2 className="font-title text-[#211D1A] font-semibold text-title-size text-center lg:text-[2.25rem]">
               Nuovi arrivi
             </h2>
             <div className="mt-4">
-              <div className="grid grid-cols-1 md:grid-cols-4 gap-4 md:gap-6">
+              <div className="grid grid-cols-1 md:grid-cols-4 gap-4 md:gap-4">
                 {prodotti.filter((item) => item.newArrivals).slice(0, 4).map((item) => (
                   <CardProdotti key={item.title} {...item} />
                 ))}
