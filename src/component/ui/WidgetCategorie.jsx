73464c8cf597bae8380fccd0bce7ffb1b7bb1336
santie96 <santi96emma@gmail.com>
2026-07-14 11:11:43 +0200

    fix

--- src/component/ui/WidgetCategorie.jsx
+++ src/component/ui/WidgetCategorie.jsx
@@ -31,7 +31,7 @@ function WidgetCategorie() {
               Uno stile per tutta la famiglia
             </h2>
             <div className="mt-4">
-              <div className="grid grid-cols-1 md:grid-cols-3 gap-4 md:gap-4">
+              <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
                 {categorie.map((item) => (
                   <CardCategorie key={item.title} {...item} />
                 ))}
